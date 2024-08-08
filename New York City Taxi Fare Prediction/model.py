8818654f55e3f8856c57c47a5398e6e8e08549d8
David Guzzi <dguzzi_10@hotmail.com>
2024-08-08 20:46:37 -0300

    DG

--- New York City Taxi Fare Prediction/model.py
+++ New York City Taxi Fare Prediction/model.py
@@ -10,3 +10,17 @@ df_2 = df.groupby(['passenger_count'])['fare_amount'].mean()
 # df_2
 
 df_2
+
+#%%
+import pandas as pd
+
+#Read train.csv
+# df = pd.read_csv('train.csv').sample(frac=0.03, random_state=7)
+
+#Get a sample, and save it
+# sample = df.to_csv(r'C:\Users\HP\OneDrive\Escritorio\David Guzzi\Github\DGKaggle\New York City Taxi Fare Prediction\train_s.csv')
+
+#Read train_s.csv and explore it
+
+df = pd.read_csv('train_s.csv')
+df.info()
